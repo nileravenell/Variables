f8af8608dd37070403041c53be3bd694268cd61f
Nile Ravenell <43184722+nileravenell@users.noreply.github.com>
2020-12-03 19:52:31 -0500

    Add files via upload

---
+++ Variables/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int testscore = 9;
+        float price = 5.99f;
+        String username = "ndizzy1";
+        System.out.println("Test Score initialized is: " + testscore);
+        System.out.println("Price tag: " + price);
+        System.out.println("Username: " + username);
+    }
+}
